606ab0fb6574c6858663962f6ee3019ba8a1a432
ecophilina <paenglis@lakeheadu.ca>
2021-12-29 22:53:04 -0800

    make function for batch simulating mpa surveys

--- analysis/functions.R
+++ analysis/functions.R
@@ -196,3 +196,182 @@ fit_geo_model <- function(surv_dat, pred_grid,
   ind$survey_abbrev <- surv_dat$survey_abbrev[1]
   ind
 }
+
+
+sim_mpa_surv <- function(surv_dat, grid,
+                       survey = "HBLL",
+                       # not sure if delta-gamma possible to sim effectively?
+                       # so for now hard coded to tweedie
+                       # family = sdmTMB::tweedie(link = "log"),
+                       mpa_increase_per_year = log(1.05), ...
+                      ) {
+  SEED <- 2938
+  set.seed(SEED)
+
+  null_df <- data.frame(
+    species_common_name = surv_dat$species_common_name[1],
+    survey_abbrev = surv_dat$survey_abbrev[1],
+    stringsAsFactors = FALSE
+  )
+
+  surv_dat <- surv_dat %>% mutate(depth_mean = mean(surv_dat$depth),
+                                  depth_sd = sd(surv_dat$depth),
+                                  depth_scaled = (depth - depth_mean)/ depth_sd,
+                                  depth_scaled2 = depth_scaled^2
+                                  )
+  # if (survey == "HBLL") surv_dat$density <- surv_dat$density_ppkm2
+  # if (survey == "SYN") surv_dat$density <- surv_dat$density_kgpm2 * 1000
+  # surv_dat$response <- surv_dat$density
+  # cutoff <- if (survey == "HBLL") 10 else 15
+  # m <- do_sdmTMB_fit(surv_dat, cutoff,
+  #                    MPA_trend = FALSE,
+  #                    family = sdmTMB::tweedie(link = "log"),
+  #                    return_model = TRUE, ...)
+
+  m0 <- fit_geo_model(surv_dat, pred_grid = grid, survey = survey,
+                      family = "tweedie",
+                      MPA_trend = FALSE,
+                      return_model = TRUE, do_fit = FALSE)
+
+  m <- try({sdmTMB::sdmTMB(
+                      # response ~ 1 + s(year, k = 5), #didn't fit as well for dogfish HBLL
+                      response ~ 0 + as.factor(year) + depth_scaled + depth_scaled2,
+                      data = m0$data,
+                      mesh = m0$spde,
+                      time = "year",
+                      family = sdmTMB::tweedie(link = "log"),
+                      # silent = FALSE,
+                      return_model = TRUE, ...)
+        })
+  # m
+
+  if (class(m)[[1]] == "try-error") return(null_df)
+  if (max(m$gradients) > 0.01) {
+    m <- try({
+      run_extra_optimization(m, newton_loops = 1L, nlminb_loops = 0L)
+    })
+  }
+  if (class(m)[[1]] == "try-error" || max(m$gradients) > 0.01) return(null_df)
+
+  b1 <- tidy(m)
+  b2 <- tidy(m, "ran_pars")
+  b <- bind_rows(b1, b2)
+
+  dat <- m$data
+  dat$year_covariate <- dat$year - min(dat$year)
+  pars <- m$tmb_obj$env$last.par.best
+  omega_s <- pars[names(pars) == "omega_s"]
+  epsilon_st <- matrix(pars[names(pars) == "epsilon_st"], ncol = length(unique(dat$year)))
+
+  s <- sdmTMB::sdmTMB_simulate(
+    formula = ~ 1 + depth_scaled + depth_scaled2 + restricted * year_covariate ,
+    data = dat,
+    mesh = m$spde,
+    family = sdmTMB::tweedie(link = "log"),
+    time = "year",
+
+    # rho = 2 * plogis(m$model$par[['ar1_phi']]) - 1, # TODO!?
+    rho = NULL,
+    sigma_O = b$estimate[b$term == "sigma_O"],
+    sigma_E = b$estimate[b$term == "sigma_E"],
+    phi = b$estimate[b$term == "phi"],
+    tweedie_p = b$estimate[b$term == "tweedie_p"],
+    range = b$estimate[b$term == "range"],
+    fixed_re = list(omega_s = omega_s, epsilon_st = NULL, zeta_s = NULL),
+    # (Intercept), restrictedTRUE, year_covariate, restrictedTRUE:year_covariate
+    B = c(mean(b[grep("year", b$term), "estimate"]),
+          b[b$term=="depth_scaled", "estimate"],
+          b[b$term=="depth_scaled2", "estimate"],
+          0, 0, mpa_increase_per_year),
+    seed = SEED
+  )
+  s <- rename(s, restricted = restrictedTRUE)
+
+  m2 <- try({
+    sdmTMB::sdmTMB(
+    observed ~ 1 + depth_scaled + depth_scaled2 + restricted * year_covariate,
+    data = s,
+    mesh = m$spde,
+    time = "year",
+    family = sdmTMB::tweedie(link = "log"),
+    # silent = FALSE,
+    # ...
+    spatial = "on",
+    spatiotemporal = "IID"
+    # spatial = "off",
+    # spatiotemporal = "AR1"
+  )
+  })
+
+  if (class(m2)[[1]] == "try-error") return(null_df)
+  if (max(m2$gradients) > 0.01) {
+    m2 <- try({
+      run_extra_optimization(m2, newton_loops = 1L, nlminb_loops = 0L)
+    })
+  }
+  if (class(m2)[[1]] == "try-error" || max(m2$gradients) > 0.05) return(null_df)
+
+  pred_grid <- grid %>% mutate(depth_scaled = (depth - surv_dat$depth_mean[1])/ surv_dat$depth_sd[1],
+                               depth_scaled2 = depth_scaled^2
+  )
+
+  pred_grid <- filter(pred_grid, year %in% unique(dat$year))
+  pred_grid$year_covariate <- pred_grid$year - min(pred_grid$year)
+
+  p <- try({predict(m2, newdata = pred_grid, sims = 300L)})
+  if (class(p)[[1]] == "try-error") return(null_df)
+
+  p_mpa <- p[pred_grid$restricted, ]
+  attr(p_mpa, "time") <- "year"
+
+  p_out <- p[!pred_grid$restricted, ]
+  attr(p_out, "time") <- "year"
+
+  ind_mpa <- get_index_sims(p_mpa)
+  ind_out <- get_index_sims(p_out)
+
+  b3 <- tidy(m2, conf.int = T)
+
+  # browser()
+
+  # # could add 4. naive GLM BACI on raw observations?
+  # m3 <- mgcv::gam(observed ~ restricted * year_covariate, data = m2$data, family = tw())
+  # # above ignores spatial correlation and temporal autocorrelation and sampling location
+  # summary(m3)
+
+
+  eta <- s %>%
+    group_by(restricted, year) %>%
+    summarise(mean_eta = mean(eta)) %>% ungroup()
+
+  ind_mpa <- ind_mpa %>% left_join(., filter(eta, restricted == 1)) %>%
+    mutate(type = "Restricted",
+           true_slope = mpa_increase_per_year,
+           m_slope = b3[b3$term=="restricted:year_covariate",2],
+           m_slope_se = b3[b3$term=="restricted:year_covariate",3],
+           m_slope_lwr = b3[b3$term=="restricted:year_covariate",4],
+           m_slope_upr = b3[b3$term=="restricted:year_covariate",5]
+           )
+
+  ind_out <- ind_out %>% left_join(., filter(eta, restricted == 0)) %>%
+     mutate(type = "Not restricted",
+            true_slope = 0,
+            m_slope = b3[b3$term=="year_covariate",2],
+            m_slope_se =b3[b3$term=="year_covariate",3],
+            m_slope_lwr = b3[b3$term=="year_covariate",4],
+            m_slope_upr = b3[b3$term=="year_covariate",5]
+            )
+  ind <- bind_rows(ind_mpa, ind_out)
+
+  # ind$true_slope <- mpa_increase_per_year
+  # ind$m_slope <- b3[b3$term=="restricted:year_covariate",2]
+  # ind$m_slope_se <- b3[b3$term=="restricted:year_covariate",3]
+  ind$species_common_name <- surv_dat$species_common_name[1]
+  ind$survey_abbrev <- surv_dat$survey_abbrev[1]
+  ind
+  # # not sure what to save?
+  # list(
+  #   mpa_increase_per_year = mpa_increase_per_year,
+  #   m = m2, # model includes simulated data
+  #   index = ind)
+}
