9429210b9da5b5044f3afddcf6eaa9cffaec4d40
Sean Anderson <sean@seananderson.ca>
2022-01-14 17:03:11 -0800

    Styler

--- analysis/rel-error-slopes.R
+++ analysis/rel-error-slopes.R
@@ -10,11 +10,9 @@ options(dplyr.summarise.inform = FALSE)
 survey <- "SYN"
 # ---------------------------------------------
 
-
 # for now using delta-gamma?
 if (survey == "HBLL") y <- readRDS(file = "data-generated/index-hbll-geo-clean.rds")
 if (survey == "SYN") y <- readRDS(file = "data-generated/index-syn-geo-clean.rds")
-
 
 # keep only species with original cv <= 1
 mean(y$orig_cv < 1)
@@ -29,18 +27,21 @@ index$species_common_name <- stringr::str_to_title(index$species_common_name)
 cv2 <- index %>%
   group_by(species_common_name, survey_abbrev, year) %>%
   summarise(
-    prop_mpa = est[type == "MPA only"]/est[type == "Status quo"],
+    prop_mpa = est[type == "MPA only"] / est[type == "Status quo"],
     cv_ratio_restr = cv[type == "Restricted"] /
       cv[type == "Status quo"],
     cv_ratio_shrunk = cv[type == "Restricted and shrunk"] /
       cv[type == "Status quo"]
   )
 
-cv_long <- cv2 %>% select(-prop_mpa) %>%
+cv_long <- cv2 %>%
+  select(-prop_mpa) %>%
   tidyr::pivot_longer(starts_with("cv"),
-                      names_to = "Restriction type", values_to = "CV ratio")
+    names_to = "Restriction type", values_to = "CV ratio"
+  )
 
-prop_mpa <- cv2 %>% select(species_common_name, survey_abbrev, year, prop_mpa) %>%
+prop_mpa <- cv2 %>%
+  select(species_common_name, survey_abbrev, year, prop_mpa) %>%
   group_by(species_common_name, survey_abbrev) %>%
   summarise(
     prop_mpa = mean(prop_mpa)
@@ -64,13 +65,16 @@ lu <- tibble(
   restr_clean = c("Same survey domain", "Shrunk survey domain")
 )
 
-cvraw <- index %>% filter(type !="MPA only") %>%
+cvraw <- index %>%
+  filter(type != "MPA only") %>%
   group_by(species_common_name, survey_abbrev, type) %>%
   summarise(
     cv_orig = mean(orig_cv),
     cv_max = max(cv),
     cv_mean = mean(cv)
-  ) %>% filter(type !="Status quo") %>% left_join(., lu)
+  ) %>%
+  filter(type != "Status quo") %>%
+  left_join(., lu)
 
 
 # bad year in SYN, removed from plot so removing here too
@@ -83,7 +87,7 @@ re <- index %>%
   mutate(est = est / exp(mean(log(est)))) %>%
   group_by(species_common_name, survey_abbrev, year) %>%
   summarise(
-    prop_mpa = est[type == "MPA only"]/est[type == "Status quo"],
+    prop_mpa = est[type == "MPA only"] / est[type == "Status quo"],
     re_restr = (est[type == "Restricted"] - est[type == "Status quo"]) / est[type == "Status quo"],
     re_shrunk = (est[type == "Restricted and shrunk"] - est[type == "Status quo"]) / est[type == "Status quo"]
   )
@@ -104,32 +108,42 @@ mare <- re_long %>%
 
 
 # rename variables for other previously calculated indices
-cvratio2 <- cvratio %>% rename(cv_ratio = est, cv_lwr =lwr, cv_upr = upr) %>% select(-`Restriction type`)
-mare2 <- mare %>% rename(mare = est, mare_lwr = lwr, mare_upr = upr) %>%
+cvratio2 <- cvratio %>%
+  rename(cv_ratio = est, cv_lwr = lwr, cv_upr = upr) %>%
+  select(-`Restriction type`)
+mare2 <- mare %>%
+  rename(mare = est, mare_lwr = lwr, mare_upr = upr) %>%
   mutate(spp_by_survey = paste(species_common_name, survey_abbrev))
 
 
 
 # mean(re_long$year)
 re_long <- re_long %>%
-  mutate(decade = (year - 2012)/10,
-         re100 = re*100,
-         spp_by_survey = paste(species_common_name,survey_abbrev)
-         )
+  mutate(
+    decade = (year - 2012) / 10,
+    re100 = re * 100,
+    spp_by_survey = paste(species_common_name, survey_abbrev)
+  )
 
 survs <- unique(re_long$survey_abbrev)
 
-re_long %>% filter(`Restriction type` == "re_restr") %>%
+re_long %>%
+  filter(`Restriction type` == "re_restr") %>%
   ggplot(aes(year, re100, colour = species_common_name)) +
-  geom_line() + facet_wrap(~survey_abbrev, dir= "v")
+  geom_line() +
+  facet_wrap(~survey_abbrev, dir = "v")
 
-index %>% filter(type == "Restricted and shrunk") %>%
+index %>%
+  filter(type == "Restricted and shrunk") %>%
   ggplot(aes(year, cv, colour = species_common_name)) +
-  geom_line() + facet_wrap(~survey_abbrev, dir= "v")
+  geom_line() +
+  facet_wrap(~survey_abbrev, dir = "v")
 
-index %>% filter(type == "Status quo") %>%
+index %>%
+  filter(type == "Status quo") %>%
   ggplot(aes(year, cv, colour = species_common_name)) +
-  geom_line() + facet_wrap(~survey_abbrev, dir= "v")
+  geom_line() +
+  facet_wrap(~survey_abbrev, dir = "v")
 
 
 # # calculate slope from fig 2 relative error plots using random effects
@@ -179,40 +193,49 @@ spp <- unique(re_long$species_common_name)
 sps <- list()
 
 
-for (i in seq_along(spp)){
-  for (j in seq_along(survs)){
-  dat <- re_long %>% filter(survey_abbrev == survs[j]) %>%
-    filter(species_common_name == spp[i])
+for (i in seq_along(spp)) {
+  for (j in seq_along(survs)) {
+    dat <- re_long %>%
+      filter(survey_abbrev == survs[j]) %>%
+      filter(species_common_name == spp[i])
 
-  d1 <- filter(dat, `Restriction type` == "re_restr")
-  m1  <- tryCatch(lm(re100 ~ 1 + decade, data = d1), error=function(err) NA)
+    d1 <- filter(dat, `Restriction type` == "re_restr")
+    m1 <- tryCatch(lm(re100 ~ 1 + decade, data = d1), error = function(err) NA)
 
-  d2 <- filter(dat, `Restriction type` == "re_shrunk")
-  m2  <- tryCatch(lm(re100 ~ 1 + decade, data = d2), error=function(err) NA)
+    d2 <- filter(dat, `Restriction type` == "re_shrunk")
+    m2 <- tryCatch(lm(re100 ~ 1 + decade, data = d2), error = function(err) NA)
 
-  m3  <- tryCatch(lm(prop_mpa ~ 1 + decade, data = d1), error=function(err) NA)
-  m4  <- tryCatch(lm(prop_mpa ~ 1 + decade, data = d2), error=function(err) NA)
+    m3 <- tryCatch(lm(prop_mpa ~ 1 + decade, data = d1), error = function(err) NA)
+    m4 <- tryCatch(lm(prop_mpa ~ 1 + decade, data = d2), error = function(err) NA)
 
-  k <- length(spp)*(j-1) + i
+    k <- length(spp) * (j - 1) + i
 
-  sps[[k]] <- tibble(
-    survey_abbrev = c(survs[j], survs[j]),
-    species_common_name = c(spp[i],spp[i]),
-    `(Intercept)` = c(tryCatch(m1$coefficients[[1]], error=function(err) NA),
-                      tryCatch(m2$coefficients[[1]], error=function(err) NA)),
-    slope_re = c(tryCatch(m1$coefficients[["decade"]], error=function(err) NA),
-              tryCatch(m2$coefficients[["decade"]], error=function(err) NA)),
-    slope_mpa = c(tryCatch(m3$coefficients[["decade"]], error=function(err) NA),
-              tryCatch(m4$coefficients[["decade"]], error=function(err) NA)),
-    restr_clean = c("Same survey domain", "Shrunk survey domain")
-  )
+    sps[[k]] <- tibble(
+      survey_abbrev = c(survs[j], survs[j]),
+      species_common_name = c(spp[i], spp[i]),
+      `(Intercept)` = c(
+        tryCatch(m1$coefficients[[1]], error = function(err) NA),
+        tryCatch(m2$coefficients[[1]], error = function(err) NA)
+      ),
+      slope_re = c(
+        tryCatch(m1$coefficients[["decade"]], error = function(err) NA),
+        tryCatch(m2$coefficients[["decade"]], error = function(err) NA)
+      ),
+      slope_mpa = c(
+        tryCatch(m3$coefficients[["decade"]], error = function(err) NA),
+        tryCatch(m4$coefficients[["decade"]], error = function(err) NA)
+      ),
+      restr_clean = c("Same survey domain", "Shrunk survey domain")
+    )
   }
 }
 coefs <- do.call(rbind, sps)
 
 # join everything together
-cvdata <- left_join(cvratio2, mare2) %>% left_join(., coefs) %>%
-  left_join(., cvraw) %>% left_join(., prop_mpa)
+cvdata <- left_join(cvratio2, mare2) %>%
+  left_join(., coefs) %>%
+  left_join(., cvraw) %>%
+  left_join(., prop_mpa)
 
 if (survey == "HBLL") saveRDS(cvdata, "data-generated/hbll-cv-w-lm-slopes.rds")
 if (survey == "SYN") saveRDS(cvdata, "data-generated/syn-cv-w-lm-slopes.rds")
@@ -225,4 +248,3 @@ if (survey == "SYN") saveRDS(cvdata, "data-generated/syn-cv-w-lm-slopes.rds")
 # (g <- ggplot(d) + geom_point(aes_string("cv_orig", "temp_bias_interp_ratio"))+ geom_hline(yintercept = 1))
 # (g <- ggplot(d) + geom_point(aes_string("cv_orig", "cv_interp_ratio"))+ geom_hline(yintercept = 1))
 # (g <- ggplot(d) + geom_point(aes_string("cv_orig", "mare_interp_ratio"))+ geom_hline(yintercept = 1))
-
