9b41fa3e6e52cacdc9ef48f9e6baa53eebb86148
ecophilina <paenglis@lakeheadu.ca>
2022-04-05 10:25:11 -0700

    start get stats for manuscript script

---
+++ analysis/06-get-values.R
@@ -0,0 +1,30 @@
+# get stats for manuscript
+library(dplyr)
+
+write_tex <- function(x, macro, ...) {
+  paste0("\\newcommand{\\", macro, "}{", x, "}") %>%
+    readr::write_lines("analysis/values.tex", append = TRUE)
+}
+
+
+paste0("% lost on each survey") %>% readr::write_lines("analysis/values.tex", append = TRUE)
+
+.d <- readRDS("data-generated/hbll-n-grid-w-restr.rds")
+write_tex(round((
+  sum(.d$restricted)/length(.d$restricted)
+) , 2), "lostHBLL")
+
+grid <- readRDS("data-generated/syn-grid-w-restr.rds")
+.d <- filter(grid, survey_abbrev == "SYN HS")
+write_tex(round((
+  sum(.d$restricted)/length(.d$restricted)
+) , 2), "lostHS")
+.d <- filter(grid, survey_abbrev == "SYN QCS")
+write_tex(round((
+  sum(.d$restricted)/length(.d$restricted)
+) , 2), "lostQCS")
+.d <- filter(grid, survey_abbrev == "SYN WCHG")
+write_tex(round((
+  sum(.d$restricted)/length(.d$restricted)
+) , 2), "lostWCHG")
+
