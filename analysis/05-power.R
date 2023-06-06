e9dd22efa0e4eef9d05d67bcc13c1195f52c63be
Sean Anderson <sean@seananderson.ca>
2023-06-05 17:32:54 -0700

    Expand power analysis to HBLL and dynamic model type

--- analysis/05-power.R
+++ analysis/05-power.R
@@ -1,3 +1,4 @@
+setwd(here::here()) # for background RStudio jobs
 library(tidyverse)
 library(sdmTMB)
 # theme_set(theme_light())
@@ -5,7 +6,8 @@ source("analysis/theme.R")
 library(future)
 plan(multisession, workers = 10L)
 
-survey <- "SYN WCHG"
+
+# survey <- "SYN WCHG"
 # spp <- "canary rockfish"
 # # spp <- "pacific ocean perch"
 # # spp <- "redbanded rockfish"
@@ -24,15 +26,16 @@ m <- metrics_wide |>
   filter(!is.na(prop_mpa)) |>
   filter(prop_mpa > 0.15) |>
   filter(est_type == "geostat") |>
-  filter(survey_abbrev == "SYN WCHG")
-m <- filter(m, !grepl("Greenstripe", species_common_name)) |>
+  filter(survey_abbrev %in% c("SYN WCHG", "HBLL OUT N"))
+m <- m |>
+  # filter(m, !grepl("Greenstripe", species_common_name)) |>
   mutate(species_common_name = tolower(species_common_name)) |>
-  select(species_common_name, orig_cv_mean) |>
+  select(species_common_name, survey_abbrev, orig_cv_mean) |>
   distinct() |>
-  arrange(species_common_name)
+  arrange(survey_abbrev, species_common_name)
 m$species_common_name[m$species_common_name == "rougheye/blackspotted rockfish"] <- "rougheye/blackspotted rockfish complex"
 
-N_ITER <- 100
+N_ITER <- 50L
 
 # spp_list <- tolower(sort(unique(m$species_common_name)))
 # spp_list <- spp_list[!grepl("greenstripe", spp_list)] # few observations, main model fails
@@ -41,15 +44,45 @@ N_ITER <- 100
 out_list <- list()
 g_list <- list()
 
-for (s in seq_len(nrow(m))) {
-  # for (s in 14:24) {
-  spp <- m[s, "species_common_name", drop = TRUE]
+for (s_i in seq_len(nrow(m))) {
+# for (s_i in 1:2) {
+  spp <- m[s_i, "species_common_name", drop = TRUE]
+  survey <- m[s_i, "survey_abbrev", drop = TRUE]
   cat(spp, "\n")
-  surv_dat <- readRDS("data-generated/dat_to_fit.rds")
+  cat(survey, "\n")
+
+  if (grepl("SYN", survey)) {
+    surv_dat <- readRDS("data-generated/dat_to_fit.rds")
+  } else {
+    surv_dat <- readRDS("data-generated/dat_to_fit_hbll.rds")
+  }
 
   # group_by(surv_dat, species_common_name) |> summarise(mpos = mean(frac_pos)) |> arrange(-mpos) |> filter(species_common_name %in% spp_list) |> as.data.frame()
 
   surv_dat <- dplyr::filter(surv_dat, survey_abbrev %in% survey, species_common_name == spp)
+
+  ########
+
+  # figure out what model was fit before:
+  f <- list.files("data-generated/indexes", pattern = ".rds", full.names = TRUE)
+  ind <- purrr::map_dfr(f, readRDS)
+  ind$cv <- NULL
+  ind$cv <- sqrt(exp(ind$se^2) - 1)
+
+  ind_spp <- filter(ind, species_common_name == spp, type == "Status quo", survey_abbrev == survey) |> select(family, anisotropy, spatiotemporal) |> distinct()
+
+  mi <- list()
+  if (ind_spp$family[1] == "tweedie") mi$family <- tweedie()
+  delta_model <- FALSE
+  if (grepl("binomial", ind_spp$family[1])) {
+    mi$family <- delta_gamma()
+  delta_model <- TRUE
+  }
+  st <- as.list(strsplit(ind_spp$spatiotemporal, ", ")[[1]])
+  if (length(st) == 1L) st <- st[[1]]
+  mi$spatiotemporal <- st
+
+  ########
 
   prep_cols_syn <- function(d) {
     d$area_swept1 <- d$doorspread_m * d$tow_length_m
@@ -62,13 +95,17 @@ for (s in seq_len(nrow(m))) {
     d$response <- d$catch_weight
     d
   }
-  # prep_cols_hbll <- function(d) {
-  #   d$offset <- log(d$hook_count)
-  #   d$response <- d$catch_count
-  #   d
-  # }
+  prep_cols_hbll <- function(d) {
+    d$offset <- log(d$hook_count)
+    d$response <- d$catch_count
+    d
+  }
 
-  surv_dat <- prep_cols_syn(surv_dat)
+  if (grepl("SYN", survey)) {
+    surv_dat <- prep_cols_syn(surv_dat)
+  } else {
+    surv_dat <- prep_cols_hbll(surv_dat)
+  }
 
   priors <- sdmTMBpriors(
     matern_s = pc_matern(range_gt = 20, sigma_lt = 5),
@@ -82,13 +119,12 @@ for (s in seq_len(nrow(m))) {
     sdmTMB(
       response ~ 0 + as.factor(year),
       data = surv_dat,
-      family = delta_gamma(),
+      family = mi$family,
       time = "year",
-      spatiotemporal = list("off", "iid"),
+      priors = priors,
+      spatiotemporal = mi$spatiotemporal,
       offset = "offset",
-      mesh = mesh_all,
-      anisotropy = FALSE,
-      silent = TRUE
+      mesh = mesh_all
     )
   sanity(fit)
 
@@ -108,52 +144,72 @@ for (s in seq_len(nrow(m))) {
     b <- bind_rows(b1, b2)
 
     omega_s <- get_pars(fit)$omega_s
-
-    s_bin <- sdmTMB_simulate(
-      formula = ~ 1 + year_covariate,
-      data = p,
-      mesh = mesh_all,
-      family = binomial(),
-      time = "year",
-      sigma_O = b$estimate[b$term == "sigma_O"],
-      # sigma_E = b$estimate[b$term == "sigma_E"],
-      sigma_E = NULL,
-      range = b$estimate[b$term == "range"],
-      fixed_re = list(omega_s = omega_s[, 1, drop = FALSE], epsilon_st = NULL, zeta_s = NULL),
-      B = c(mean(b[grep("year", b$term), "estimate", drop = TRUE]), 0),
-      seed = 42 * i
-    )
-
-    suppressMessages({
-      b1 <- tidy(fit, model = 2)
-      b2 <- tidy(fit, "ran_pars", model = 2)
-    })
-    b <- bind_rows(b1, b2)
     epsilon_st <- get_pars(fit)$epsilon_st
 
-    s_pos <- sdmTMB_simulate(
-      formula = ~ 1 + year_covariate,
-      data = p,
-      mesh = mesh_all,
-      family = Gamma(link = "log"),
-      time = "year",
-      sigma_O = b$estimate[b$term == "sigma_O"],
-      sigma_E = b$estimate[b$term == "sigma_E"],
-      range = b$estimate[b$term == "range"],
-      phi = b$estimate[b$term == "phi"],
-      fixed_re = list(omega_s = omega_s[, 2, drop = FALSE], epsilon_st = epsilon_st, zeta_s = NULL),
-      B = c(mean(b[grep("year", b$term), "estimate", drop = TRUE]), change_per_year),
-      seed = 421 * i
-    )
+    if (grepl("binomial", ind_spp$family[1])) {
+      s_bin <- sdmTMB_simulate(
+        formula = ~ 1 + year_covariate,
+        data = p,
+        mesh = mesh_all,
+        family = binomial(),
+        time = "year",
+        sigma_O = b$estimate[b$term == "sigma_O"],
+        # sigma_E = b$estimate[b$term == "sigma_E"],
+        sigma_E = if ("sigma_E" %in% b$term) b$estimate[b$term == "sigma_E"] else NULL,
+        range = b$estimate[b$term == "range"],
+        fixed_re = list(omega_s = omega_s[, 1, drop = FALSE], epsilon_st = epsilon_st[,,1,drop=FALSE], zeta_s = NULL),
+        B = c(mean(b[grep("year", b$term), "estimate", drop = TRUE]), 0),
+        seed = 42 * i
+      )
 
-    # note offset is not set
-    s1 <- select(s_bin, year, X, Y, mu_bin = mu, obs_bin = observed)
-    s2 <- select(s_pos, mu_pos = mu, obs_pos = observed)
+      suppressMessages({
+        b1 <- tidy(fit, model = 2)
+        b2 <- tidy(fit, "ran_pars", model = 2)
+      })
+      b <- bind_rows(b1, b2)
 
-    s <- bind_cols(s1, s2) |> bind_cols(select(surv_dat, restricted))
-    s <- mutate(s, obs = obs_bin * obs_pos, mu = mu_bin * mu_pos)
-    # hist(s$obs)
-    # head(s)
+      s_pos <- sdmTMB_simulate(
+        formula = ~ 1 + year_covariate,
+        data = p,
+        mesh = mesh_all,
+        family = Gamma(link = "log"),
+        time = "year",
+        sigma_O = b$estimate[b$term == "sigma_O"],
+        sigma_E = if ("sigma_E" %in% b$term) b$estimate[b$term == "sigma_E"] else NULL,
+        range = b$estimate[b$term == "range"],
+        phi = b$estimate[b$term == "phi"],
+        fixed_re = list(omega_s = omega_s[, 2, drop = FALSE], epsilon_st = epsilon_st[,,2,drop=FALSE], zeta_s = NULL),
+        B = c(mean(b[grep("year", b$term), "estimate", drop = TRUE]), change_per_year),
+        seed = 421 * i
+      )
+
+      # note offset is not set
+      s1 <- select(s_bin, year, X, Y, mu_bin = mu, obs_bin = observed)
+      s2 <- select(s_pos, mu_pos = mu, obs_pos = observed)
+
+      s <- bind_cols(s1, s2) |> bind_cols(select(surv_dat, restricted))
+      s <- mutate(s, obs = obs_bin * obs_pos, mu = mu_bin * mu_pos)
+      # hist(s$obs)
+      # head(s)
+
+    } else { # tweedie
+      s <- sdmTMB_simulate(
+        formula = ~ 1 + year_covariate,
+        data = p,
+        mesh = mesh_all,
+        family = tweedie(),
+        time = "year",
+        sigma_O = b$estimate[b$term == "sigma_O"],
+        sigma_E = b$estimate[b$term == "sigma_E"],
+        tweedie_p = b$estimate[b$term == "tweedie_p"],
+        range = b$estimate[b$term == "range"],
+        phi = b$estimate[b$term == "phi"],
+        fixed_re = list(omega_s = omega_s, epsilon_st = epsilon_st, zeta_s = NULL),
+        B = c(mean(b[grep("year", b$term), "estimate", drop = TRUE]), change_per_year),
+        seed = 421 * i
+      )
+      s <- select(s, year, X, Y, mu, obs = observed) |> bind_cols(select(surv_dat, restricted))
+    }
     sr <- filter(s, !restricted)
 
     # library(ggplot2)
@@ -177,8 +233,8 @@ for (s in seq_len(nrow(m))) {
 
     fit_squo <- try({
       sdmTMB(list(obs ~ 1, obs ~ year_zero),
-        data = s, family = delta_gamma(),
-        spatial = "on", spatiotemporal = list("off", "iid"), time = "year", mesh = mesh_all
+        data = s, family = mi$family,
+        spatial = "on", spatiotemporal = mi$spatiotemporal, time = "year", mesh = mesh_all
         # priors = priors
       )
     })
@@ -186,16 +242,16 @@ for (s in seq_len(nrow(m))) {
     datr <- filter(s, !restricted)
     fit_rest <- try({
       sdmTMB(list(obs ~ 1, obs ~ year_zero),
-        data = datr, family = delta_gamma(),
-        spatial = "on", spatiotemporal = list("off", "iid"), time = "year", mesh = meshr
+        data = datr, family = mi$family,
+        spatial = "on", spatiotemporal = mi$spatiotemporal, time = "year", mesh = meshr,
         # priors = priors
       )
     })
 
     if (class(fit_squo) != "try-error" && class(fit_rest) != "try-error") {
       ret <- bind_rows(
-        mutate(broom::tidy(fit_squo, conf.int = TRUE, model = 2L), type = "status quo"),
-        mutate(broom::tidy(fit_rest, conf.int = TRUE, model = 2L), type = "restricted and shrunk")
+        mutate(tidy(fit_squo, conf.int = TRUE, model = if (delta_model) 2L else 1L), type = "status quo"),
+        mutate(tidy(fit_rest, conf.int = TRUE, model = if (delta_model) 2L else 1L), type = "restricted and shrunk")
       ) |>
         filter(grepl("year", term)) |>
         mutate(true_effect = change_per_year, i = i)
@@ -225,7 +281,7 @@ for (s in seq_len(nrow(m))) {
   # )
   # FRAC_TEST <- 0.5
 
-  cv <- m[s, "orig_cv_mean", drop = TRUE]
+  cv <- m[s_i, "orig_cv_mean", drop = TRUE]
 
   FRAC_TEST <- if (cv > 0.5) {
     0.3
@@ -261,7 +317,7 @@ for (s in seq_len(nrow(m))) {
 
   out$species_common_name <- spp
   out$fract_tested <- FRAC_TEST
-  out_list[[s]] <- out
+  out_list[[s_i]] <- out
   group_by(out, type) |>
     filter(!is.na(conf.high)) |>
     summarise(
@@ -287,12 +343,12 @@ for (s in seq_len(nrow(m))) {
     facet_wrap(~type) +
     ggtitle(spp)
 
-  g_list[[s]] <- g
+  g_list[[s_i]] <- g
 }
 
 plan(sequential)
 
-saveRDS(out_list, file = "data-generated/power-cached-output.rds")
+saveRDS(out_list, file = "data-generated/power-cached-output-2023-06-05.rds")
 
 d <- dplyr::bind_rows(out_list)
 
@@ -365,7 +421,7 @@ select(x, -power_diff) |>
     x = `status quo`,
     y = sp,
     yend = sp,
-    colour = as.factor(paste0((1 - fract_tested)*100, "%"))
+    colour = as.factor(paste0((1 - fract_tested) * 100, "%"))
   )) +
   scale_colour_viridis_d(end = 0.85, option = "C") +
   geom_segment(arrow = arrow(length = unit(6, "pt")), key_glyph = "arrow_left") +
@@ -378,7 +434,7 @@ select(x, -power_diff) |>
   labs(colour = "Simulated decline") +
   theme(
     legend.position = c(0.28, 0.12),
-    plot.margin = margin(11/2, 11/2 + 2, 11/2, 11/2),
+    plot.margin = margin(11 / 2, 11 / 2 + 2, 11 / 2, 11 / 2),
     axis.title = element_text(size = 10)
   )
 
