1d34265b470296cd37a64042ca7c25bc4d3334d8
Sean Anderson <sean@seananderson.ca>
2023-08-18 10:28:15 -0700

    Delete old files

--- analysis/plot-utils.R
+++
@@ -1,9 +0,0 @@
-# restricted_cols <- c("#FC8D62", "#8DA0CB")
-# colour_pal <- c("gray50", restricted_cols)
-# line_pal <- c("dotted", "solid", "solid")
-# restricted_labels <- c("Extrapolated", "Shrunk")
-#
-# .pal <- viridisLite::viridis(3, begin = 0, end = 0.95)
-# names(.pal) <- c("SYN QCS, SYN HS", "HBLL OUT N", "SYN WCHG")
-#
-# ggplot2::theme_set(ggsidekick::theme_sleek())
